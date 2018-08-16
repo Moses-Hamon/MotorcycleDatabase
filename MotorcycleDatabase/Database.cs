647f26fbd18c34f99742182c32b330fd29481720
Moses-Hamon <moses_hamon@bigpond.com>
2018-08-17 09:23:20 +1000

    added Database class and db schema

---
+++ MotorcycleDatabase/Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//adding in the usings
+using System.IO;
+using System.Data.SQLite;
+using Dapper;
+
+namespace MotorcycleDatabase
+{
+    //add and make the class static
+    public static class Database
+    {
+        const string DB_NAME = "moto.db";
+        static string CONNECTION_STRING;
+        
+
+        static Database()
+        {
+            var DB_PATH = HttpContext.Current.Server.MapPath($"~/App_Data/{DB_NAME}");
+            CONNECTION_STRING = $"Data Source={DB_PATH}";
+
+            var SCHEMA_PATH = HttpContext.Current.Server.MapPath($"~/App_Data/schema.txt");
+            var query = File.ReadAllText(SCHEMA_PATH);
+
+            //i'll be back
+        }
+
+        public static SQLiteConnection GetSQLiteConnection()
+        {
+            return new SQLiteConnection(CONNECTION_STRING);
+        }
+
+    }
+}
